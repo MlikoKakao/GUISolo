e866b39e3bccc1170089818e38a6380814c2a1e1
PC <mattewczlp@seznam.cz>
2025-03-01 15:20:19 +0100

    First

---
+++ src/Character.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Character {
+    Map<String,Integer> Stats = new HashMap<>();
+
+    public Character() {
+        Stats.put("INT", 2);
+    }
+}
