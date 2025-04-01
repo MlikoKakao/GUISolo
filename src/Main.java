a34027f985fcfdeeddbd251561921b2a095f4660
PC <mattewczlp@seznam.cz>
2025-04-01 11:32:50 +0200

    Complete revamp for better GUI

--- src/Main.java
+++ src/Main.java
@@ -6,67 +6,13 @@ import java.awt.event.ActionEvent;
 
 public class Main {
     public static void main(String [] args){
-        //Character reference
-        Character character = new Character();
-        Character.IncInt();
 
-        //JFrame start
-        JFrame frame = new JFrame("Solo");
-        frame.setSize(500,800);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-
-        //Menu bar
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileMenu = new JMenu("File");
-        JMenu editMenu = new JMenu("Edit");
-        JMenuItem openItem = new JMenuItem("Open");
-        JMenuItem exitItem = new JMenuItem("Exit");
-        fileMenu.add(openItem);
-        fileMenu.addSeparator();
-        fileMenu.add(exitItem);
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-
-        //Panel with button
-        JPanel panel = new JPanel();
-        JButton button = new JButton("Add Stat");
-
-        //Button action
-        button.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    JOptionPane.showMessageDialog(frame, "Button clicked");
-                    Character.Stats.put("INT",Character.Stats.get("INT")+1);
-                }
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new SoloGUI().setVisible(true);
+            }
         });
-
-        //Another panel with text
-        JPanel textPanel = new JPanel();
-        JLabel label2 = new JLabel(character.toString());
-        textPanel.add(label2);
-        textPanel.add(new JButton("Aaa"));
-
-
-        //Label
-        JLabel label = new JLabel(character.toString());
-        textPanel.add(label);
-
-
-        //Set layout
-        frame.setLayout(new BorderLayout());
-        frame.add(panel, BorderLayout.CENTER);
-        frame.add(textPanel,BorderLayout.SOUTH);
-        panel.add(button,BorderLayout.EAST);
-
-
-        frame.setJMenuBar(menuBar);
-        frame.setVisible(true);
-        //JFrame end
-
-
-
-
-
 
 
 
---
+++ src/SoloGUI.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SoloGUI extends JFrame {
+    public SoloGUI(){
+        super("Solo Leveling");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,800);
+
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+
+        addGuiComponents();
+
+    }
+    private void addGuiComponents(){
+        JTextField searchTF = new JTextField();
+        searchTF.setBounds(15,15,351,45);
+
+        //Search text field added
+        searchTF.setFont(new Font("Dialog", Font.PLAIN,24));
+        add(searchTF);
+
+        //Search button
+        JButton searchBT = new JButton("Search");
+        searchBT.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        searchBT.setBounds(375,13,100,45);
+        add(searchBT);
+
+        //INT label
+        JLabel intLbl = new JLabel("INT");
+        intLbl.setFont(new Font("Dialog", Font.PLAIN,40));
+        intLbl.setBounds(50,100,80,50);
+        add(intLbl);
+        intLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+}
