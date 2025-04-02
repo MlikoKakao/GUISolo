3a7113bbc11b9db465769e73cd4860d502771c26
PC <mattewczlp@seznam.cz>
2025-04-02 14:06:19 +0200

    Instance, functioning buttons for 2 stats, need save and load to and from file

--- src/Character.java
+++ src/Character.java
@@ -2,19 +2,48 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Character {
-    static Map<String,Integer> Stats = new HashMap<>();
+    private int intelligence;
+    private int strength;
+    private int sense;
+    private int ability;
+    private int durability;
 
-    public Character() {
-        Stats.put("INT", 2);
-        System.out.println(Stats);
+   public Character(int intelligence,int strength,int sense,
+   int ability,int durability){
+       this.intelligence = intelligence;
+       this.strength = strength;
+       this.sense = sense;
+       this.ability = ability;
+       this.durability = durability;
+   }
+    public int getIntelligence(){
+       return intelligence;
     }
-    public static void IncInt(){
-        Stats.put("INT",Stats.get("INT")+1);
+    public void setIntelligence(int intelligence){
+       this.intelligence = intelligence;
     }
-    @Override
-    public String toString(){
-        return "Stats: "+Stats;
+    public int getStrength(){
+        return strength;
     }
-
-
+    public void setStrength(int strength){
+        this.strength = strength;
+    }
+    public int getSense(){
+        return sense;
+    }
+    public void setSense(int sense){
+        this.sense = sense;
+    }
+    public int getAbility(){
+        return ability;
+    }
+    public void setAbility(int ability){
+        this.ability = ability;
+    }
+    public int getDurability(){
+        return durability;
+    }
+    public void setDurability(int durability){
+        this.durability = durability;
+    }
 }
--- src/SoloGUI.java
+++ src/SoloGUI.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SoloGUI extends JFrame {
+    private Character character;
     public SoloGUI(){
         super("Solo Leveling");
+        character = new Character(0,0,0,0,0);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,800);
@@ -16,6 +20,7 @@ public class SoloGUI extends JFrame {
 
     }
     private void addGuiComponents(){
+
         JTextField searchTF = new JTextField();
         searchTF.setBounds(65,15,351,45);
 
@@ -37,11 +42,52 @@ public class SoloGUI extends JFrame {
         add(intLbl);
 
 
+
+        JLabel intStatLbl = new JLabel(String.valueOf(character.getIntelligence()));
+        intStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
+        intStatLbl.setBounds(450,100,70,40);
+        intStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(intStatLbl);
+
+        JButton incIntBt = new JButton("+");
+        incIntBt.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        incIntBt.setFont(new Font("Dialog", Font.PLAIN,27));
+        incIntBt.setBounds(525,100,50,40);
+        incIntBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                character.setIntelligence(character.getIntelligence()+1);
+                intStatLbl.setText(String.valueOf(character.getIntelligence()));
+
+            }
+        });
+        add(incIntBt);
+
         JLabel strLbl = new JLabel("STR");
         strLbl.setFont(new Font("Dialog", Font.PLAIN,40));
         strLbl.setBounds(50,200,90,40);
         strLbl.setBorder(BorderFactory.createLineBorder(Color.black));
         add(strLbl);
+
+        JLabel strStatLbl = new JLabel((String.valueOf(character.getStrength())));
+        strStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
+        strStatLbl.setBounds(450,200,70,40);
+        strStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(strStatLbl);
+
+        JButton incStrBt = new JButton("+");
+        incStrBt.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        incStrBt.setFont(new Font("Dialog", Font.PLAIN,27));
+        incStrBt.setBounds(525,200,50,40);
+        incStrBt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                character.setStrength(character.getStrength()+1);
+                strStatLbl.setText(String.valueOf(character.getStrength()));
+
+            }
+        });
+        add(incStrBt);
 
         JLabel senLbl = new JLabel("SEN");
         senLbl.setFont(new Font("Dialog", Font.PLAIN,40));
@@ -49,11 +95,23 @@ public class SoloGUI extends JFrame {
         senLbl.setBorder(BorderFactory.createLineBorder(Color.black));
         add(senLbl);
 
+        JLabel senStatLbl = new JLabel((String.valueOf(character.getSense())));
+        senStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
+        senStatLbl.setBounds(450,300,70,40);
+        senStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(senStatLbl);
+
         JLabel apLbl = new JLabel("APP");
         apLbl.setFont(new Font("Dialog", Font.PLAIN,40));
         apLbl.setBounds(50,400,90,40);
         apLbl.setBorder(BorderFactory.createLineBorder(Color.black));
         add(apLbl);
+
+        JLabel apStatLbl = new JLabel((String.valueOf(character.getAbility())));
+        apStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
+        apStatLbl.setBounds(450,400,70,40);
+        apStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(apStatLbl);
 
         JLabel durLbl = new JLabel("DUR");
         durLbl.setFont(new Font("Dialog", Font.PLAIN,40));
@@ -61,31 +119,7 @@ public class SoloGUI extends JFrame {
         durLbl.setBorder(BorderFactory.createLineBorder(Color.black));
         add(durLbl);
 
-        JLabel intStatLbl = new JLabel("0");
-        intStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
-        intStatLbl.setBounds(450,100,70,40);
-        intStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
-        add(intStatLbl);
-
-        JLabel strStatLbl = new JLabel("0");
-        strStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
-        strStatLbl.setBounds(450,200,70,40);
-        strStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
-        add(strStatLbl);
-
-        JLabel senStatLbl = new JLabel("0");
-        senStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
-        senStatLbl.setBounds(450,300,70,40);
-        senStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
-        add(senStatLbl);
-
-        JLabel apStatLbl = new JLabel("0");
-        apStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
-        apStatLbl.setBounds(450,400,70,40);
-        apStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
-        add(apStatLbl);
-
-        JLabel durStatLbl = new JLabel("0");
+        JLabel durStatLbl = new JLabel((String.valueOf(character.getDurability())));
         durStatLbl.setFont(new Font("Dialog", Font.PLAIN,40));
         durStatLbl.setBounds(450,500,70,40);
         durStatLbl.setBorder(BorderFactory.createLineBorder(Color.black));
